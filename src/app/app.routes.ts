65d1f70f378e215a64bb234e544f814f1554c56f
pranayteja-g <gadi.pranayteja@gmail.com>
2024-12-29 16:52:24 +0530

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,24 @@ import { authGuard } from './guards/auth.guard';
 import { StudentDashboardComponent } from './pages/student-dashboard/student-dashboard.component';
 import { EmployeeDashboardComponent } from './pages/employee-dashboard/employee-dashboard.component';
 import { ForbiddenComponent } from './pages/forbidden/forbidden.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { StudentComponent } from './pages/student/student.component';
+import { FeesComponent } from './pages/fees/fees.component';
+import { ExamresultsComponent } from './pages/examresults/examresults.component';
+import { AdminOverviewComponent } from './pages/admin-overview/admin-overview.component';
+import { SalaryComponent } from './pages/salary/salary.component';
 
 export const routes: Routes = [
 
     { path: "", redirectTo: "/login", pathMatch: "full" },
     { path: "login", component: LoginComponent },
     { path: "forbidden", component: ForbiddenComponent },
+    { path: "employee", component: EmployeeComponent },
+    { path: "student", component: StudentComponent },
+    { path: "fees", component: FeesComponent },
+    { path: "examresults", component: ExamresultsComponent },
+    { path: "admin", component: AdminOverviewComponent },
+    { path: "salary", component: SalaryComponent },
 
     {
         path: "admindashboard",
